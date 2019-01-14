350371ffb72365b18cb65ffa0799584763d71f61
xsd08tfu <smith.myles+github@gmail.com>
2019-01-14 18:46:14 +0000

    Update api.php

--- api/api.php
+++ api/api.php
@@ -1,17 +1,12 @@
 <?php
-
+$path = $_SERVER['DOCUMENT_ROOT'];
+include ($path."/mysqli_connect.php");
 if($_GET['action']=="reading"){
-  if($_SERVER['REQUEST_METHOD']=="PUT"){
-    $data = json_decode(file_get_contents("php://input"),$assoc=TRUE);
-  } else {
-    if (file_get_contents("php://input")!=NULL){
-      $data = json_decode(file_get_contents("php://input"),$assoc=TRUE);
-    } else {
-      $data = json_decode($_POST['data'],$assoc=TRUE);
-    }
-  };
+  $data = json_decode(file_get_contents("php://input"),$assoc=TRUE);
   print_r($data);
+
 } else {
   echo $_GET['action'];
 };
+mysqli_close($link);
 ?>
