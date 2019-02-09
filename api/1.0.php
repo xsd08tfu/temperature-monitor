44fab1ab5d0dc67f68b6fa15faa1dba05ba6c61d
xsd08tfu <smith.myles+github@gmail.com>
2019-02-09 09:56:55 +0000

    v1.0

--- api/1.0.php
+++ api/1.0.php
@@ -2,16 +2,27 @@
 $path = $_SERVER['DOCUMENT_ROOT'];
 include ($path."/mysqli_connect.php");
 if ($stmt = mysqli_prepare($link, "SELECT `api_key_id`,`api_key` FROM $db_name.`api_keys` WHERE api_key = ?")) {
-  /* bind parameters for markers */
-    mysqli_stmt_bind_param($stmt, "s", $_GET['key']);
-  /* execute query */
-    mysqli_stmt_execute($stmt) or die(mysqli_error($link));
-  /* bind result variables */
-    mysqli_stmt_bind_result($stmt, $api_key_id, $api_key);//Bind to variables.
-  /* fetch value */
-    mysqli_stmt_fetch($stmt);
-    mysqli_stmt_close($stmt);
-  };
+/* bind parameters for markers */
+  mysqli_stmt_bind_param($stmt, "s", $_GET['key']);
+/* execute query */
+  mysqli_stmt_execute($stmt) or die(mysqli_error($link));
+/* bind result variables */
+  mysqli_stmt_bind_result($stmt, $api_key_id, $api_key);//Bind to variables.
+/* fetch value */
+  mysqli_stmt_fetch($stmt);
+  mysqli_stmt_close($stmt);
+};
+$now = time();
+function returnJson ($query) {
+  global $link;
+  $result = mysqli_query($link,$query) or die(mysqli_error($link));
+  $array = mysqli_fetch_all($result, MYSQLI_ASSOC);
+  foreach ($array as $key => $value) {
+    $array[$key]['date'] = date("d-m-Y H:i", $value['timestamp']);
+  }
+  header('Content-Type: application/json');
+  echo json_encode($array);
+}
 if(isset($api_key_id)){
   $query = "SELECT * FROM $db_name.`sensors` WHERE `api_key` = '$api_key'"; //Get sensor array
   $result = mysqli_query($link,$query) or die(mysqli_error($link));
@@ -30,38 +41,17 @@ if(isset($api_key_id)){
         mysqli_stmt_close($stmt);
       };
     } elseif($_GET['action']=="temperature"){
-      $now = time();
       $start = strtotime($_GET['period']." ago");
       $query = "SELECT `timestamp`,`temperature` FROM $db_name.`readings` WHERE `sensor_id` = '".$_GET['sensor']."' AND `timestamp` BETWEEN $start AND $now"; //Get sensor array
-      $result = mysqli_query($link,$query) or die(mysqli_error($link));
-      $array = mysqli_fetch_all($result, MYSQLI_ASSOC);
-      foreach ($array as $key => $value) {
-        $array[$key]['date'] = date("d-m-Y H:i", $value['timestamp']);
-      }
-      header('Content-Type: application/json');
-      echo json_encode($array);
+      returnJson($query);
     } elseif($_GET['action']=="pressure"){
-      $now = time();
       $start = strtotime($_GET['period']." ago");
-      $query = "SELECT `timestamp`,`pressure` FROM $db_name.`readings` WHERE `sensor_id` = '".$_GET['sensor']."'"; //Get sensor array
-      $result = mysqli_query($link,$query) or die(mysqli_error($link));
-      $array = mysqli_fetch_all($result, MYSQLI_ASSOC);
-      foreach ($array as $key => $value) {
-        $array[$key]['date'] = date("d-m-Y H:i", $value['timestamp']);
-      }
-      header('Content-Type: application/json');
-      echo json_encode($array);
+      $query = "SELECT `timestamp`,`pressure` FROM $db_name.`readings` WHERE `sensor_id` = '".$_GET['sensor']."' AND `timestamp` BETWEEN $start AND $now"; //Get sensor array
+      returnJson($query);
     } elseif($_GET['action']=="light"){
-      $now = time();
       $start = strtotime($_GET['period']." ago");
-      $query = "SELECT `timestamp`,`lux` FROM $db_name.`readings` WHERE `sensor_id` = '".$_GET['sensor']."'"; //Get sensor array
-      $result = mysqli_query($link,$query) or die(mysqli_error($link));
-      $array = mysqli_fetch_all($result, MYSQLI_ASSOC);
-      foreach ($array as $key => $value) {
-        $array[$key]['date'] = date("d-m-Y H:i", $value['timestamp']);
-      }
-      header('Content-Type: application/json');
-      echo json_encode($array);
+      $query = "SELECT `timestamp`,`lux` FROM $db_name.`readings` WHERE `sensor_id` = '".$_GET['sensor']."' AND `timestamp` BETWEEN $start AND $now"; //Get sensor array
+      returnJson($query);
     } else {
       http_response_code(400);
       echo "Unknown API Method";
